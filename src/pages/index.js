0ab33514b430ac12f064aa30590775726253cc0a
zimrick <richard@zcreativelabs.com>
2024-09-04 12:26:22 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+export default function IndexPage() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "system-ui, sans-serif",
+      }}
+    >
+      {"NetZero Pathfinders"}
+    </div>
+  )
+}
