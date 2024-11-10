bdcfc4309308ecec6ac0b7de5f5a5fa770cf75f4
Cristina Poiata <cris_poiata@Cristinas-MacBook-Pro.local>
2024-11-10 17:36:12 +0100

    added overflow

--- src/components/MethodologyReportBanner/index.js
+++ src/components/MethodologyReportBanner/index.js
@@ -18,6 +18,7 @@ export default function MethodologyReportBanner() {
       bgImage="/images/banner.jpg"
       color="white"
       py={0}
+      overflow="hidden"
     >
       <BannerHeader borderColor="transparent">
         <Heading fontSize={["3xl", null, "4xl", "5xl"]} fontWeight={600}>
